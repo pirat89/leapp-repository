c4fd9fcedeb88e63c3e438f854ae4ba55ff44f5e
Petr Stodulka <pstodulk@redhat.com>
2020-11-19 22:12:10 +0100

    Add the checkinitramfstasks actor
    It's possible that some actors could provide conflicting requests
    (e.g. install system dracut module A & custom (from own path) dracut
    module A). This actor prevents system to upgrade if conflicting tasks
    for the upgrade or target initramfses are detected.

---
+++ repos/system_upgrade/el7toel8/actors/initramfs/checkinitramfstasks/libraries/checkinitramfstasks.py
@@ -0,0 +1,66 @@
+from collections import defaultdict
+import os
+
+from leapp import reporting
+from leapp.libraries.stdlib import api
+from leapp.models import UpgradeInitramfsTasks, TargetInitramfsTasks
+
+DRACUT_MOD_DIR = '/usr/lib/dracut/modules.d/'
+SUMMARY_DRACUT_FMT = (
+    'The requested dracut modules for the initramfs are in conflict.'
+    ' At least one dracut module is specified to be installed from'
+    ' multiple paths. The list of conflicting dracut module names'
+    ' with paths is listed below: {}'
+)
+
+
+def _printable_modules(conflicts):
+    list_separator_fmt = '\n    - '
+    for name, paths in conflicts.items():
+        output = ['{}{}: {}'.format(list_separator_fmt, name, paths)]
+    return ''.join(output)
+
+
+def _treat_path(dmodule):
+    """
+    In case the path is not set, set the expected path of the dracut module.
+    """
+    if not dmodule.module_path:
+        return os.path.join(DRACUT_MOD_DIR, dmodule.name)
+    return dmodule.module_path
+
+
+def _detect_dracut_modules_conflicts(msgtype):
+    """
+    Return dict of modules with conflicting
+    """
+    dracut_modules = defaultdict(set)
+    for msg in api.consume(msgtype):
+        for dmodule in msg.include_dracut_modules:
+            dracut_modules[dmodule.name].add(_treat_path(dmodule))
+    return {key: val for key, val in dracut_modules.items() if len(val) > 1}
+
+
+def process():
+    # TODO: it's not ideal code but...
+    conflicts = _detect_dracut_modules_conflicts(UpgradeInitramfsTasks)
+    if conflicts:
+        report = [
+            reporting.Title('Conflicting requirements of dracut modules for the upgrade initramfs'),
+            reporting.Summary(SUMMARY_DRACUT_FMT.format(_printable_modules(conflicts))),
+            reporting.Severity(reporting.Severity.HIGH),
+            reporting.Tags([reporting.Tags.SANITY]),
+            reporting.Flags([reporting.Flags.INHIBITOR]),
+        ]
+        reporting.create_report(report)
+
+    conflicts = _detect_dracut_modules_conflicts(TargetInitramfsTasks)
+    if conflicts:
+        report = [
+            reporting.Title('Conflicting requirements of dracut modules for the target initramfs'),
+            reporting.Summary(SUMMARY_DRACUT_FMT.format(_printable_modules(conflicts))),
+            reporting.Severity(reporting.Severity.HIGH),
+            reporting.Tags([reporting.Tags.SANITY]),
+            reporting.Flags([reporting.Flags.INHIBITOR]),
+        ]
+        reporting.create_report(report)
